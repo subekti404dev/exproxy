17a42637185c2849c0d425a1634aa9c00eb8b310
Imanuel Urip Subekti <imanuel.subekti@efishery.com>
2023-09-30 03:10:57 +0700

    feat: increase version

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
   --tag subekti13/exproxy:latest \
-  --tag subekti13/exproxy:1.0.0-alpha.2 \
+  --tag subekti13/exproxy:1.0.0-alpha.3 \
   --progress=plain \
   .
